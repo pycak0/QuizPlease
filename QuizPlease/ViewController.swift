e4af71b2e924c3f37d2265a623be21d931149a4b
pycak0 <vladisrusakov@yandex.ru>
2020-07-30 12:13:40 +0300

    Initial Commit

---
+++ QuizPlease/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuizPlease
+//
+//  Created by Владислав on 30.07.2020.
+//  Copyright © 2020 Владислав. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
