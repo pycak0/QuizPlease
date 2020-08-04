87991999c6880c9e3a84bee64e8a135794a65b7b
pycak0 <vladisrusakov@yandex.ru>
2020-08-04 17:31:28 +0300

    Prepared other modules for configuration

---
+++ QuizPlease/Modules/HomeGame/HomeGameRouter.swift
@@ -0,0 +1,9 @@
+//
+//  HomeGameRouter.swift
+//  QuizPlease
+//
+//  Created by Владислав on 04.08.2020.
+//  Copyright © 2020 Владислав. All rights reserved.
+//
+
+import Foundation
---
+++ QuizPlease/Modules/Profile/ProfileInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileInteractor.swift
+//  QuizPlease
+//
+//  Created by Владислав on 04.08.2020.
+//  Copyright © 2020 Владислав. All rights reserved.
+//
+
+import Foundation
