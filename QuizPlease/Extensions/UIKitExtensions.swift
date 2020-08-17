a899afab82e4473c46913420f9d078a9dfad14b7
pycak0 <vladisrusakov@yandex.ru>
2020-08-18 02:30:39 +0300

    fixed layout issue with searchField

--- QuizPlease/Extensions/UIKitExtensions.swift
+++ QuizPlease/Extensions/UIKitExtensions.swift
@@ -129,15 +129,14 @@ public extension UITextField{
     
     //MARK:- Set Image
     
-    ///- parameter side: Preferred width and height of image's `CGrect` (the shape is a square). Is set to `frame.height` value if bigger than it
+    ///- parameter side: Preferred width and height of image's `CGrect` (the shape is a square).
     ///- parameter textPadding: Image inset from textField's text
     ///- parameter edgePadding: Image inset from textField's left side. The default is equal to `textPadding` value
     func setImage(_ image: UIImage?, side: CGFloat = 20, textPadding: CGFloat = 16, edgePadding: CGFloat? = nil) {
-        let side = min(side, bounds.height)
+        
         let yOffset = (bounds.height - side) / 2
         let iconView = UIImageView(frame: CGRect(x: edgePadding ?? textPadding, y: yOffset, width: side, height: side))
         iconView.image = image
-        print(image == nil)
         
         let containerWidth = textPadding + (edgePadding ?? textPadding) + side
         let container = UIView(frame: CGRect(x: 0, y: 0, width: containerWidth, height: bounds.height))
